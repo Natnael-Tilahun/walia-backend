68ffa97d57b54d11fefed2cb2402b6461a89d9ba
natnael-tilahun <natnaeltilahun97@gmail.com>
2023-10-23 14:32:17 +0300

    Create user table in the use page

--- src/app/(dashboard)/users/page.tsx
+++ src/app/(dashboard)/users/page.tsx
@@ -1,11 +1,101 @@
-import React from "react";
+import { DataTable } from "@/components/data-table";
+import { User, columns } from "./columns";
 
-function Users() {
+async function getData(): Promise<User[]> {
+  return [
+    {
+      id: "728ed52f",
+      email: "anatnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "bnatnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "cnatnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "dnatnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "yosefteshome@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "abebekebed@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+    {
+      id: "728ed52f",
+      email: "natnaeltilahun97@gmail.com",
+      phone: "0933654654",
+      fullName: "Natnael Tilahun",
+      status: "user",
+    },
+  ];
+}
+
+export default async function Users() {
+  const data = await getData();
+
   return (
-    <div className="flex w-full min-h-screen flex-col justify-between p-10">
-      Users
+    <div className="px-5 lg:px-16 mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
   );
 }
-
-export default Users;
